90c74961972fe3a7c07c563effb267020349f2ed
Melissa Rosemery Yauri Machaca <myaurima@everis.com>
2020-06-29 01:26:00 -0500

    feat: component title

--- src/components/List/Title.js
+++ src/components/List/Title.js
@@ -1,29 +1,51 @@
 import React, { useState } from 'react'
 import { Typography, InputBase } from '@material-ui/core';
+import MoreHoriz from '@material-ui/icons/MoreHoriz'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyle = makeStyles((theme) => ({
+    editableTitleContainer: {
+        marginLeft: theme.spacing(1),
+        display: 'flex'
+    },
     editTableTitle: {
-        marginLeft: theme.spacing(1)
-    }
+        flexGrow: 1
+    },
+    input: {
+        fontSize: '1.2rem',
+        fontWeight: 'bold',
+        margin: theme.spacing(1),
+        '&:focus': {
+            background: '#ddd',
+        },
+    },
+
 }))
 
 export default function Title() {
-    const { open, setOpen } = useState(false);
+    const [open, setOpen] = useState(false);
     const classes = useStyle();
     return (
         <div>
             {open ?
                 (<div>
-                    <InputBase value="Todo" />
+                    <InputBase value="Todo"
+                        inputProps={{
+                            className: classes.input
+                        }}
+                        fullWidth
+                        onBlur={() => setOpen(!open)}
+                    />
                 </div>) :
-                (<div>
+                (<div className={classes.editableTitleContainer}>
                     <Typography
+                        className={classes.editTableTitle}
                         onClick={() => setOpen(!open)}
                         className={classes.editTableTitle}
                     >
                         Todo
                     </Typography>
+                    <MoreHoriz />
                 </div>)}
         </div>
     )
